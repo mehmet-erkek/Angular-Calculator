66f8bde9c8bccf95e84537f65f7146e40a40f45c
roundel35 <mehmeterkek@windowslive.com>
2024-02-15 21:34:27 +0300

    equals

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -8,10 +8,49 @@ import { Component } from '@angular/core';
 export class CalculatorComponent {
 
   visibleSidebar : boolean = true;
-  value1: number = 0;
+  value1 = '0';
+  firstValue: number | null = null;
+  action: string | null = null;
 
   openCalculator(){
     this.visibleSidebar = true;
   }
+
+  numClick(val:any) {
+    if (this.value1 === '0') {
+      this.value1 = val.toString();
+    } else {
+      this.value1 = `${this.value1}${val}`;
+    }
+  }
+
+  oper(action:any) {
+    this.firstValue = parseFloat(this.value1);
+    this.action = action;
+    this.value1 = ' ';
+  }
+
+  calculate() {
+    let a : any = this.firstValue;
+    let b : any = parseFloat(this.value1);
+
+    let result : any;
+    if (this.action === 'm') {
+      result = a * b;
+    }
+    else if (this.action === 'd') {
+      result = a / b;
+    }
+    else if (this.action === 'a') {
+      result = a + b;
+    }
+    else if (this.action === 's') {
+      result = a - b;
+    }
+
+    this.firstValue = result;
+    this.value1 = result.toString();
+  }
+
   Closed(){}
 }
